02f996cd41979262cc601ce00dcc6d6149e55790
johnfreyes <jhonfer96@hotmail.com>
2022-09-24 00:23:49 -0500

    sync package section: add conditional variable broadcast

--- 16-cond-broadcast/main.go
+++ 16-cond-broadcast/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 var sharedRsc = make(map[string]interface{})
@@ -11,35 +10,45 @@ var sharedRsc = make(map[string]interface{})
 func main() {
 	var wg sync.WaitGroup
 
+	mu := sync.Mutex{}
+	c := sync.NewCond(&mu)
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
-		//TODO: suspend goroutine until sharedRsc is populated.
+		// suspend goroutine until sharedRsc is populated.
 
+		c.L.Lock()
 		for len(sharedRsc) == 0 {
-			time.Sleep(1 * time.Millisecond)
+			c.Wait()
 		}
 
 		fmt.Println(sharedRsc["rsc1"])
+		c.L.Unlock()
 	}()
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
-		//TODO: suspend goroutine until sharedRsc is populated.
+		// suspend goroutine until sharedRsc is populated.
 
+		c.L.Lock()
 		for len(sharedRsc) == 0 {
-			time.Sleep(1 * time.Millisecond)
+			c.Wait()
 		}
 
 		fmt.Println(sharedRsc["rsc2"])
+		c.L.Unlock()
 	}()
 
+	c.L.Lock()
 	// writes changes to sharedRsc
 	sharedRsc["rsc1"] = "foo"
 	sharedRsc["rsc2"] = "bar"
+	c.Broadcast()
+	c.L.Unlock()
 
 	wg.Wait()
 }
