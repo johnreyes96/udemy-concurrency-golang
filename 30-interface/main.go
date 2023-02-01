dd375c7bf2b20a32c8ed1d0cfcc8fe6b38ee1ca0
John Reyes <jhonfer96@hotmail.com>
2023-02-01 12:38:57 -0500

    bonus section: add interface

--- 30-interface/main.go
+++ 30-interface/main.go
@@ -14,8 +14,22 @@ func (m *Metal) Density() float64 {
 	return m.mass / m.volume
 }
 
+type Gas struct {
+	pressure      float64
+	temperature   float64
+	molecularMass float64
+}
+
+func (g *Gas) Density() float64 {
+	return (g.molecularMass * g.pressure) / (0.0821 * (g.temperature + 273))
+}
+
+type Dense interface {
+	Density() float64
+}
+
 // IsDenser - compare density of two objects
-func IsDenser(a, b *Metal) bool {
+func IsDenser(a, b Dense) bool {
 	return a.Density() > b.Density()
 }
 
@@ -28,5 +42,23 @@ func main() {
 		fmt.Println("gold has higher density than silver")
 	} else {
 		fmt.Println("silver has higher density than gold")
+	}
+
+	oxygen := Gas{
+		pressure:      5,
+		temperature:   27,
+		molecularMass: 32,
+	}
+	hydrogen := Gas{
+		pressure:      1,
+		temperature:   0,
+		molecularMass: 2,
+	}
+
+	result = IsDenser(&oxygen, &hydrogen)
+	if result {
+		fmt.Println("oxygen has higher density than hydrogen")
+	} else {
+		fmt.Println("hydrogen has higher density than oxygen")
 	}
 }
