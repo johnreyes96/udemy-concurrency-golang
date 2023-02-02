a0459b42a7892cf770f75a9d99b980eb04480076
John Reyes <jhonfer96@hotmail.com>
2023-02-02 11:42:16 -0500

    bonus section: add implementation custm stringer

--- 32-interface-string/main.go
+++ 32-interface-string/main.go
@@ -7,7 +7,10 @@ type user struct {
 	email string
 }
 
-// TODO: Implement custom formating for user struct values.
+// Implement custom formating for user struct values.
+func (u user) String() string {
+	return fmt.Sprintf("%s <%s>", u.name, u.email)
+}
 
 func main() {
 	u := user{
