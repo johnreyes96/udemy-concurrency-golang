004a1f581ef6ce972415bc60dd666b67dc96ef3e
John Reyes <jhonfer96@hotmail.com>
2023-02-02 11:33:56 -0500

    bonus section: add implementation custom interface

--- 31-interface-byte-counter/main.go
+++ 31-interface-byte-counter/main.go
@@ -5,8 +5,12 @@ import "fmt"
 // ByteCounter type
 type ByteCounter int
 
-// TODO: Implement Write method for ByteCounter
+// Implement Write method for ByteCounter
 // to count the number of bytes written.
+func (bc *ByteCounter) Write(p []byte) (n int, err error) {
+	*bc += ByteCounter(len(p))
+	return len(p), nil
+}
 
 func main() {
 	var b ByteCounter
